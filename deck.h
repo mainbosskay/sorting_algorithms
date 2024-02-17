55d786f31f409d3433a17d8ad542966e01379674
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2024-02-17 14:04:30 -0800

    deck

--- deck.h
+++ deck.h
@@ -1,111 +1,51 @@
-#include "deck.h"
+#ifndef DECK_header
+#define DECK_header
 
-void swap_nd(deck_node_t *frst, deck_node_t *secnd);
-int compr_order(const card_t *frst, const card_t *secnd);
-void insertion_sort_list(deck_node_t **list);
+#include <stdio.h>
+
+/**
+ *enum kind_e - Enumeration representing differnt playing card suits
+ * @SPADE: spade suit (0)
+ * @HEART: heart suit (1)
+ * @CLUB: club suit (2)
+ * @DIAMOND: diamond suit (3)
+ */
+
+typedef enum kind_e
+{
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
+} kind_t;
+
+/**
+ * struct card_s - Playing card
+ *
+ * @value: Value of the card
+ * From "Ace" to "King"
+ * @kind: Kind of the card
+ */
+typedef struct card_s
+{
+	const char *value;
+	const kind_t kind;
+} card_t;
+
+/**
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
+typedef struct deck_node_s
+{
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
+} deck_node_t;
+
 void sort_deck(deck_node_t **deck);
 
-/**
- * swap_nd - Swapping two nodes in a doubly linked list
- * @frst: pointer to the first node
- * @secnd: pointer to the second node
- * Return: it is void
- */
-
-void swap_nd(deck_node_t *frst, deck_node_t *secnd)
-{
-	if (frst->prev)
-		frst->prev->next = secnd;
-	if (secnd->next)
-		secnd->next->prev = frst;
-	frst->next = secnd->next;
-	secnd->prev = frst->prev;
-	frst->prev = secnd;
-	secnd->next = frst;
-}
-
-/**
- * compr_order - Comparing two playing cards for their order
- * @frst: pointer to the frist card
- * @secnd: pointer to the second card
- * Return: 1 if frst < secnd and 0 otherwise
- */
-
-int compr_order(const card_t *frst, const card_t *secnd)
-{
-	char *stck1;
-	char *stck2;
-	char *rankval[] = {"King", "Queen", "Jack", "10", "9", "8", "7", "6",
-		"5", "4", "3", "2", "Ace"};
-	int frstv = 0;
-	int secndv = 0;
-	int s = 0;
-
-	for (s = 0 ; s < 13 ; s++)
-	{
-		for (stck1 = (char *)frst->value, stck2 = rankval[s] ; *stck1 &&
-				*stck1 == *stck2 ; ++stck1, ++stck2)
-			;
-		if (*stck1 == 0 && *stck2 == 0)
-		{
-			frstv = s;
-			break;
-		}
-	}
-	for (s = 0 ; s < 13 ; s++)
-	{
-		for (stck1 = (char *)secnd->value, stck2 = rankval[s] ; *stck1 &&
-				*stck1 == *stck2 ; ++stck1, ++stck2)
-			;
-		if (*stck1 == 0 && *stck2 == 0)
-		{
-			secndv = s;
-			break;
-		}
-	}
-	if (frst->kind == secnd->kind)
-		return (frstv < secndv);
-	return (frst->kind > secnd->kind);
-}
-
-/**
- * insertion_sort_list - Sorting doubly linked list with insertion
- * @list: double pointer to the head
- * Return: it is void
- */
-
-void insertion_sort_list(deck_node_t **list)
-{
-	deck_node_t *currentnd, *innernd;
-
-	if (!list || !*list || !(*list)->next)
-		return;
-	currentnd = (*list)->next;
-	while (currentnd != NULL)
-	{
-		innernd = currentnd;
-		currentnd = currentnd->next;
-		while (innernd != NULL && innernd->prev != NULL)
-		{
-			if (compr_order(innernd->prev->card, innernd->card))
-			{
-				swap_nd(innernd->prev, innernd);
-				if (!innernd->prev)
-					*list = innernd;
-			}
-			else
-				innernd = innernd->prev;
-		}
-	}
-}
-
-/**
- * sort_deck - Sorting deck of cards with insert sort
- * @deck: double pointer to the head
- * Return: it is void
- */
-
-void sort_deck(deck_node_t **deck)
-{
-	insertion_sort_list(deck);
-}
+#endif
